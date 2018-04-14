b518e87aff89f15931def8b365fa371c562a29c6
fangzhang(James) <fangzhang@ebay.com>
2018-04-14 12:21:30 +0800

    Add backend structure

---
+++ backend/src/main/java/com/running/coins/RunningclubApplication.java
@@ -0,0 +1,12 @@
+package com.running.coins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunningclubApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/running/coins/RunningclubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.running.coins;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunningclubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
